bdb161f443a19a05da92cfe01ecaa83af5029eb8
Turab <turab.violin@gmail.com>
2026-06-06 18:06:32 +0400

    Added Main Menu

--- Turkic Game Jam 2026/Assets/Scripts/DragItem.cs
+++ Turkic Game Jam 2026/Assets/Scripts/DragItem.cs
@@ -1,61 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class DragItem : MonoBehaviour
-{
-    private Camera mainCamera;
-    private Rigidbody2D rb;
-    private Vector2 velocity = Vector2.zero;
-
-    public float followTime = 0.1f;
-    private bool isDragging = false;
-    private GameObject currentDraggedObject = null;
-
-    void Awake()
+    public class DragItem : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        mainCamera = Camera.main;
-    }
+        private Camera mainCamera;
+        private Rigidbody2D rb;
+        private Vector2 velocity = Vector2.zero;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        public float followTime = 0.1f;
+        private bool isDragging = false;
+        private GameObject currentDraggedObject = null;
+
+        void Awake()
         {
-            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
-
-            if (hit.collider != null && hit.collider.gameObject == gameObject)
-            {
-                isDragging = true;
-                currentDraggedObject = hit.collider.gameObject;
-                EventManager.DraggingProcess(true, currentDraggedObject);
-            }
-            if (hit.collider != null && hit.collider.CompareTag("Water"))
-            {
-                WaterDropletAI waterDropletAI = hit.collider.gameObject.GetComponent<WaterDropletAI>();
-                waterDropletAI.Die();
-            }
+            rb = GetComponent<Rigidbody2D>();
+            mainCamera = Camera.main;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+
+                if (hit.collider != null && hit.collider.gameObject == gameObject)
+                {
+                    isDragging = true;
+                    currentDraggedObject = hit.collider.gameObject;
+                    EventManager.DraggingProcess(true, currentDraggedObject);
+                }
+                if (hit.collider != null && hit.collider.CompareTag("Water"))
+                {
+                    WaterDropletAI waterDropletAI = hit.collider.gameObject.GetComponent<WaterDropletAI>();
+                    waterDropletAI.Die();
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (isDragging)
+                {
+                    isDragging = false;
+                    EventManager.DraggingProcess(false, currentDraggedObject);
+                }
+            }
+            
+        }
+
+        void FixedUpdate()
         {
             if (isDragging)
             {
-                isDragging = false;
-                EventManager.DraggingProcess(false, currentDraggedObject);
+                Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 nextPos = Vector2.SmoothDamp(rb.position, mouseWorldPos, ref velocity, followTime);
+                rb.MovePosition(nextPos);
             }
         }
-        
     }
-
-    void FixedUpdate()
-    {
-        if (isDragging)
-        {
-            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 nextPos = Vector2.SmoothDamp(rb.position, mouseWorldPos, ref velocity, followTime);
-            rb.MovePosition(nextPos);
-        }
-    }
-}
